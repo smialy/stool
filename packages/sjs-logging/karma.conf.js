8b4ee6e1a3bef428479684a4833b83f7725c1abc
Piotr Smialkowski <p.smialkowski@gmail.com>
2018-03-10 18:33:41 +0100

    Add npm tests command

--- packages/sjs-logging/karma.conf.js
+++ packages/sjs-logging/karma.conf.js
@@ -23,9 +23,8 @@ module.exports = function(config) {
         colors: true,
 
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome', 'Firefox'],
-        singleRun: false
+        browsers: ['Chrome', 'Firefox']
     });
 };
