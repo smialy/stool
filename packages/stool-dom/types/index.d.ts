5fa17f8ceac5dc55ba106278329ce295c3ca9747
Piotr Smialkowski <p.smialkowski@gmail.com>
2019-08-27 23:40:25 +0200

    Update deps

--- packages/stool-dom/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function ready(): Promise<any>;
-export declare function classNames(...args: any[]): string;
-export declare function findParent(element: HTMLElement, selector: string): HTMLElement | null;
