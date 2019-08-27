b9afb1a538b9b6d092dd7d550da0c3b1991fa20d
Piotr Smialkowski <p.smialkowski@gmail.com>
2019-08-27 23:23:02 +0200

    Remove pkg folder

--- packages/stool-core/pkg/dist-types/function.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const noop: () => void;
-export declare function debounce(fn: () => void, wait: number): () => void;
-export declare function tr(...args: any[]): any;
-export declare const clone: (o: any) => any;
