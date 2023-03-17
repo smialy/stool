6233a29dab3252e7c0a4d7cbc48610d5d04c1a99
Piotr Smialkowski <p.smialkowski@gmail.com>
2023-03-17 22:46:09 +0100

    [micropack] Fix default config name

--- packages/micropack/src/consts.mjs
+++ packages/micropack/src/consts.mjs
@@ -1,8 +1,10 @@
+export const DEFAULT_CONFIG_NAME = 'micropack.json';
+
 export const DEFAULT_OPTIONS = {
     cwd: process.cwd(),
     dev: false,
     watch: false,
-    configFile: '.micropack.json',
+    configFile: DEFAULT_CONFIG_NAME,
     modern: true,
     sourcemap: true,
     include: [],
