efe9ad90c4d31d4f702cea4b7d3de0d31870719c
Piotr Smialkowski <p.smialkowski@gmail.com>
2023-06-23 21:39:03 +0200

    [dom] Update deps

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,5 +1,4 @@
-import { Observable } from './Observable';
-
+import { Observable } from '@stool/core';
 
 export function ready(): Promise<void> {
     return new Promise(resolve => {
@@ -56,14 +55,6 @@ export function findParent(element: HTMLElement, selector: string): HTMLElement|
     return null;
 }
 
-export function fromEvent(element: HTMLElement, eventName: string): Observable {
-    return new Observable(observer => {
-        let handler = event => observer.next(event);
-        element.addEventListener(eventName, handler, true);
-        return () => element.removeEventListener(eventName, handler, true);
-    });
-}
-
 export function importStyles(styles: string[] | string, name: string = '@stool/dom'): () => void {
     styles = Array.isArray(styles) ? styles : [styles];
     const element = document.createElement('style');
@@ -71,4 +62,12 @@ export function importStyles(styles: string[] | string, name: string = '@stool/d
     element.innerHTML = styles.join("\n");
     document.head.appendChild(element);
     return () => element?.parentNode?.removeChild(element);
+}
+
+export function fromEvent(element: HTMLElement, eventName: string): Observable<EventTarget> {
+    return new Observable(observer => {
+        let handler = event => observer.next(event);
+        element.addEventListener(eventName, handler, true);
+        return () => element.removeEventListener(eventName, handler, true);
+    });
 }
